47b1ba13b097084f858fbd4889aad527cad7d8fc
Luis Henrique Porto Menezes <128551788+luishmenezes@users.noreply.github.com>
2024-08-23 22:03:34 +0000

    aula

---
+++ POO UNICAP/metodos/Carro.java
@@ -0,0 +1,13 @@
+public class Carro{
+    String montadora;
+    String modelo;
+    int ano;
+
+    Carro(String montadora, String modelo, int ano){
+        this.montadora = montadora;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    Carro(){};
+}
---
+++ POO UNICAP/metodos/main.java
@@ -0,0 +1,7 @@
+public class main{
+    public static void main(String[] args) {
+        Carro c1 = new Carro("Kia","Cerato",2020);
+
+        System.out.println(c1.montadora + " " +  c1.modelo + " " +  c1.ano);
+    }
+}
