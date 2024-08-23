db758fccbccc64673c638f5ae7e0493e0d47a876
Luis Henrique Porto Menezes <128551788+luishmenezes@users.noreply.github.com>
2024-08-23 22:39:07 +0000

    class e methods

--- POO UNICAP/metodos/Carro.java
+++ POO UNICAP/metodos/Carro.java
@@ -2,12 +2,47 @@ public class Carro{
     String montadora;
     String modelo;
     int ano;
+    Boolean estaLigado;
+    double velocidade;
 
     Carro(String montadora, String modelo, int ano){
         this.montadora = montadora;
         this.modelo = modelo;
         this.ano = ano;
+        this.estaLigado = false;
+        this.velocidade = 0;
     }
 
-    Carro(){};
+    Carro(){
+        this.estaLigado = false;
+        this.velocidade = 0;
+
+    };
+
+    void ligar(){
+        this.estaLigado = true;
+        System.out.println("O carro está ligado !");
+    };
+
+    void desligar(){
+        this.estaLigado = false;
+        System.out.println("O carro foi desligado !");
+    }
+
+    void buzinar(){
+        System.out.println("BEEEEP BEEEEP BEEEEEP");
+    }
+    
+    void acelerar(){
+        this.velocidade = velocidade +20;
+        
+        System.out.println("A velocidade atual é: " + velocidade);
+        
+    }
+
+    void frear(){
+        this.velocidade = velocidade -20;
+        System.out.println("A velocidade atual é: " + velocidade);
+    }
+
 }
--- POO UNICAP/metodos/main.java
+++ POO UNICAP/metodos/main.java
@@ -2,6 +2,14 @@ public class main{
     public static void main(String[] args) {
         Carro c1 = new Carro("Kia","Cerato",2020);
 
+
         System.out.println(c1.montadora + " " +  c1.modelo + " " +  c1.ano);
+
+        c1.ligar();
+        c1.desligar();
+        c1.buzinar();
+        c1.acelerar();
+        c1.acelerar();
+        c1.frear();
     }
 }
